3dce4f2d23121a62875c48e2f4425cf13ab07deb
nignanthomas <nignanthomas@gmail.com>
2018-10-26 07:42:49 +0300

    create Class User in user.ts

---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+  constructor(
+    public photourl:string,
+    public userprofile:string, 
+    public bio:string,
+    public followers:string,
+    public following:string,
+  ) {}
+}
