d070a3413a9c101199b749ff03e174f2f5e71530
nignanthomas <nignanthomas@gmail.com>
2018-10-26 18:10:21 +0300

    create the repoRequest function in the user-http service

--- src/app/user-http/user-request.service.ts
+++ src/app/user-http/user-request.service.ts
@@ -4,6 +4,7 @@ import { Response } from '@angular/http';
 
 import { environment } from '../../environments/environment';
 import { User } from '../user';
+import { Repo } from '../repo-class/repo';
 
 @Injectable(
   // providedIn: 'root'
@@ -12,13 +13,15 @@ export class UserRequestService {
 
   user: User;
   repo: Repo;
+  arrayRepo:[];
   constructor(private http:HttpClient) {
     this.user = new User("", "", "", 0, 0, 0);
-    // this.repo = new Repo("", "", "");
+    this.repo = new Repo("", "", "");
   }
 
   private userInput = "";
-  private githubData:any = "";
+  // private githubData:any = "";
+  // private githubData:any = "";
 
 
   userRequest(userInput) {
@@ -40,22 +43,27 @@ export class UserRequestService {
 
 
 
-//   repoRequest(userInput) {
-//
-//     this.http.get("https://api.github.com/users/" + userInput + "/repos?access_token=" + environment.accessToken).subscribe((response)=>{
-//     const repoData=response;
-//
-//     this.user.photoUrl = userData["avatar_url"];
-//     this.user.userProfile = userData["login"];
-//     this.user.bio = userData["bio"];
-//     this.user.numRepos = userData["public_repos"];
-//     this.user.followers = userData["followers"];
-//     this.user.following = userData["following"];
-//     console.log(userData);
-//   })
-//
-//
-// } //end repoRequest
+  repoRequest(userInput) {
+
+    this.http.get("https://api.github.com/users/" + userInput + "/repos?access_token=" + environment.accessToken).subscribe((response)=>{
+      const reposData=response;
+
+      reposData.forEach(repoData){
+
+        this.repo.appName = repoData["name"];
+        this.repo.repoLink = repoData["html_url"];
+        this.repo.description = repoData["description"];
+
+        this.arrayRepo.push(this.repo);
+
+        console.log(repoData);
+      }
+      console.log(arrayRepo)
+
+    })
+
+
+  } //end repoRequest
 
 }
 
